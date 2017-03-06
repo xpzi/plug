88ef5ef66f38541e34db52e3a3ee7acac790b4cf
yjp1918360798 <1918360798@qq.com>
2017-03-06 16:52:21 +0800

    优化 stor.js

--- storage/README.md
+++ storage/README.md
@@ -1,5 +1,6 @@
 ##  y_storage
 
+storagejs :  storage操作的小插件
 
 ### 有哪些功能
 
@@ -11,12 +12,15 @@
 
 ### 使用方法 ：
 
-setOnStorageListener( key,  _key, function )
+setOnStorageListener( key,  _key, func )
 ```
 		key: storage的key
 		_key: stor响应的标识
-		function： 响应函数的名字
-    	setOnStorageListener( key,  _key, function );
+		func: 响应函数
+			func( event, data );  
+			// event,  storage原生事件对象
+			// data, 包含旧的数据和新的数据
+    	
 ```
 
 
@@ -25,7 +29,7 @@ set( key, value, _key );
 		key: storage的key
 		value: 要设置的值
 		_key: 要触发的回调标识
-    	set( key, value, _key );
+			_key,可以是字符串触发单个  可以是字符串数组，触发多个
 ```
 
 get( key )
--- storage/stor.js
+++ storage/stor.js
@@ -1,32 +1,5 @@
 /*
-    storagejs :  storage操作的小插件
-
-.md
-    有哪些功能
-
-    1. 可以往  onstorage 这个方法 添加删除方法
-    2. 在storage改变时（有多个回调方法的时候），指定回调的某个方法，或某几个方法， 或者不执行 onstorage的回调方法
-    3. 在存储时，可以以任何数据类型存储，读取时，返回存储时的数据类型
-    4. 保留原始的访问方式
-
-
-    使用方法 ：
-
-		storName: storage的key
-		_key: stor响应的标识
-		function： 响应函数的名字
-    	setOnStorageListener( storName,  _key, function );
-
-
-		key: storage的key
-		value: 要设置的值
-		_key: 要触发的回调标识
-    	set( key, value, _key );
-
-		
-		// 获取 storage
-    	get( key );
-    
+    storagejs :  storage操作的小插件 
 */
 
 
@@ -108,8 +81,8 @@
             } else{
                 switch( typeof _key ){
                     case 'object': 
-                        for( var key in _key ){
-                            this.onstorages[ev.key][key] && this.onstorages[ev.key][key](ev, data);
+                        for( var i = 0; i < _key.length; i++ ){
+                            this.onstorages[ev.key][_key[i]] && this.onstorages[ev.key][_key[i]](ev, data);
                         };
                         break;
                     case 'string': 
